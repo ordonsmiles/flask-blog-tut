2c4a0c0427fd216fcfdda66c2eba6ced39665ece
Jordyn Shaw <ordon@nervous-system.local>
2024-11-01 13:27:38 -0500

    Added fuctionality to create posts

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request, url_for, flash, redirect, abo
 # make a Flask application object called app
 app = Flask(__name__)
 app.config["DEBUG"] = True
+app.config['SECRET_KEY'] = 'your secret key'
 
 
 
@@ -42,10 +43,23 @@ def create():
     # determine if the page is being requested with POST or GET request
     if request.method == 'POST':
         # get the title that was submitted 
-        
+        title = request.form['title']
+        content = request.form['content']
+
         # display an error message if content or title is not submitted
         # else, make a database connection and insert the blog post content
-
+        if not title:
+            flash('Title is required')
+        elif not content:
+            flash('Content is required')
+        else: 
+            conn = get_db_connection()
+            # insert data into db
+            conn.execute('INSERT INTO posts (title, content) VALUES (?, ?)', (title, content))
+            conn.commit()
+            conn.close()
+            return redirect(url_for('index'))
+        
     return render_template('create.html')
 
 app.run(port=5008)
