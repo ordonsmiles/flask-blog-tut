b29e9d8be6311a0ad4d63d8d4e36060399258c29
Jordyn Shaw <ordon@nervous-system.local>
2024-11-01 13:17:43 -0500

    Updated flask rotues and displayed posts

---
+++ app.py
@@ -0,0 +1,51 @@
+import sqlite3
+from flask import Flask, render_template, request, url_for, flash, redirect, abort
+
+# make a Flask application object called app
+app = Flask(__name__)
+app.config["DEBUG"] = True
+
+
+
+# Function to open a connection to the database.db file
+def get_db_connection():
+    # create connection to the database
+    conn = sqlite3.connect('database.db')
+    
+    # allows us to have name-based access to columns
+    # the database connection will return rows we can access like regular Python dictionaries
+    conn.row_factory = sqlite3.Row
+
+    #return the connection object
+    return conn
+
+
+# use the app.route() decorator to create a Flask view function called index()
+@app.route('/')
+def index():
+    # get a connection to db
+    conn = get_db_connection()
+
+    # execute a query to read all posts from the posts table in the db
+    posts = conn.execute('SELECT * FROM posts').fetchall()
+
+    # close the connection 
+    conn.close()
+
+    # send the posts to the index.html template to be displayed
+    return render_template('index.html', posts=posts)
+
+
+# route to create a post
+@app.route('/create/', methods=('GET', 'POST'))
+def create():
+    # determine if the page is being requested with POST or GET request
+    if request.method == 'POST':
+        # get the title that was submitted 
+        
+        # display an error message if content or title is not submitted
+        # else, make a database connection and insert the blog post content
+
+    return render_template('create.html')
+
+app.run(port=5008)
